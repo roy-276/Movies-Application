ee0f12c7cc926c2ddbb104a2d8ef5a7f6b2a740c
roy-276 <maingiroy95@gmail.com>
2023-04-07 07:05:30 +0300

    Movie List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      Title: 'John Wick',
+      Year: '2014',
+      imdbID: 'tt2911666',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'John Wick: Chapter 2',
+      Year: '2017',
+      imdbID: 'tt4425200',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300.jpg',
+    },
+    {
+      Title: 'John Wick: Chapter 3 - Parabellum',
+      Year: '2019',
+      imdbID: 'tt6146586',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg',
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movies-app">
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,14 @@
+const MovieList = (props) => {
+  return (
+    <>
+      // Map through the movies array and render an image for each movie
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m3">
+          <img src={movie.Poster} alt="Movie Poster" />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
