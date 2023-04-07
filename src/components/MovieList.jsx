4e56b148cb63d54b0b02792c28c3dac2bc798f37
roy-276 <maingiroy95@gmail.com>
2023-04-07 07:54:00 +0300

    Implementing MovieListHeading and SearchBox

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 const MovieList = (props) => {
   return (
     <>
-      // Map through the movies array and render an image for each movie
+      {/* Map through the movies array and render an image for each movie */}
       {props.movies.map((movie, index) => (
         <div className="image-container d-flex justify-content-start m3">
           <img src={movie.Poster} alt="Movie Poster" />
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,13 @@
+const SearchBox = (props) => {
+  return (
+    <div className="col col-sm-4">
+      <input
+        className="form-control"
+        value={props.value}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Search for a movie..."
+      />
+    </div>
+  );
+};
+export default SearchBox;
